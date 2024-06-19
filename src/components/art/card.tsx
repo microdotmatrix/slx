31fc057d5e1db9cb75e5839adb1c3f134ff0146d
microdotmatrix <liquid.knives.2@gmail.com>
2024-06-19 16:30:36 -0500

    pathname issues

--- src/components/art/card.tsx
+++ src/components/art/card.tsx
@@ -19,7 +19,7 @@ export const GalleryCard = ({
   if (process.env.NODE_ENV === "development") {
     siteUrl = "http://localhost:3000";
   } else {
-    siteUrl = getURL(pathname ? pathname : "", false);
+    siteUrl = pathname;
   }
   // const siteUrl = getURL(pathname ? pathname : "", false);
   const url = new URL(`${siteUrl}/art/${category}`);
