7e526de2010842db1dd151eeef4c4fc63ef1a727
microdotmatrix <liquid.knives.2@gmail.com>
2024-06-19 16:17:19 -0500

    fixing pathname in modal component

--- src/components/art/card.tsx
+++ src/components/art/card.tsx
@@ -20,7 +20,7 @@ export const GalleryCard = ({
     siteUrl = getURL(pathname ? pathname : "", false);
   }
   // const siteUrl = getURL(pathname ? pathname : "", false);
-  const url = new URL(pathname ? pathname : "", `${siteUrl}/art/${category}`);
+  const url = new URL(`${siteUrl}/art/${category}`);
 
   url.searchParams.set("modal", "true");
   url.searchParams.set("id", item.id.toString());
